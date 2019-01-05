5a133a7782e76afa058d5b358fdff5314d17911d
Едиге Нуркайыр Маратулы <@corp.itstep>
2019-01-05 13:51:01 +0600

    1

---
+++ IdentityLesson/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityLesson.Startup))]
+namespace IdentityLesson
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
